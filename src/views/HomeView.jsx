7dfa6573dd16de81ebd016252ca3cfb956ea9d54
Alexandra Vermillion <vermillionxela@gmail.com>
2025-08-21 18:20:24 -0600

    Changes to Home and Info Parents pictures and text. Added clickable lessons to Home.

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,29 +1,55 @@
 import { useState } from "react"
-import SSKHome from "../assets/SSK-home.jpg"
+import SSKHospital from "../assets/SSK-hospital.jpg"
 import { Link } from 'react-router-dom'
 
 export const HomeView = () => {
 
     const [isImageLoaded, setIsImageLoaded] = useState(false)
+    const TeachCard = ({ icon, color, title, titleUrl, titleNode, delay = 0 }) => (
+        <div
+            className={`${color} rounded-xl shadow-lg p-4 flex flex-col items-center justify-center text-center gap-2 h-28 transition-transform duration-300 hover:scale-105`}
+            data-aos="fade-up"
+            data-aos-delay={delay}
+        >
+            <div className="text-3xl">{icon}</div>
+
+            {titleNode ? (
+                <div className="font-semibold text-base sm:text-base">
+                    {titleNode}
+                </div>
+            ) : titleUrl ? (
+                <a
+                    href={titleUrl}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="font-semibold text-base sm:text-base underline"
+                >
+                    {title}
+                </a>
+            ) : (
+                <div className="font-semibold text-base sm:text-base">{title}</div>
+            )}
+        </div>
+    )
 
     return (
-        <div className="flex flex-col items-center bg-[#F0E9D8]">
+        <div className="flex flex-col items-center">
             <div className="relative w-full -mt-4">
                 <div
                     className={`absolute inset-0 w-full h-full bg-[#F0E9D8] transition-opacity duration-700 
                         ${isImageLoaded ? "opacity-0" : "opacity-100"}`}
                 />
                 <img
-                    src={SSKHome}
-                    alt="Group of kids"
+                    src={SSKHospital}
+                    alt="Photo of UCSF BCH"
                     onLoad={() => setIsImageLoaded(true)}
                     className={`w-full object-cover max-h-[600px] transition-opacity duration-700 
                         ${isImageLoaded ? "opacity-100" : "opacity-0"}`}
                 />
-                <div className="absolute top-5 left-8 w-[85%] sm:left-20 sm:w-[75%] md:left-24 md:w-[65%] md:max-w-2xl">
+                <div className="absolute top-5 left-4 w-[85%] sm:left-8 sm:w-[65%] md:left-10 md:w-[60%] md:max-w-2xl">
 
                     <p
-                        className="text-lg sm:text-4xl md:text-5xl mt-3 font-bold leading-tight text-white drop-shadow-[0_1px_2px_rgba(0,0,0,0.15)]"
+                        className="text-lg sm:text-3xl md:text-4xl lg:text-5xl mt-3 font-bold leading-tight text-white drop-shadow-[0_1px_2px_rgba(0,0,0,0.15)]"
                         data-aos="fade-up"
                         data-aos-delay="300"
                         data-aos-duration="1000"
@@ -40,45 +66,117 @@ export const HomeView = () => {
                     data-aos="fade-up"
                     data-aos-duration="400">
                     <h1 className="text-2xl font-semibold mb-2 -mt-4">About the Program</h1>
-                    <p className="text-base  leading-relaxed -mb-4">
-                        Safe Skin Kids is a UCSF student-led initiative that teaches students about skin,
-                        hair, and nail health through hands-on lessons and interactive activities. Created
-                        and run by medical students at UCSF School of Medicine, the program introduces
-                        dermatology in a way that's engaging, empowering, and age-appropriate. Each session
-                        promotes confidence, empathy, and inclusivity while helping students learn the basics
-                        of dermatology in a fun, supportive environment.
-                    </p>
+                    <div className="text-base space-y-4 leading-relaxed -mb-6">
+                        <p>
+                            Safe Skin Kids is a UCSF student-led initiative that teaches children the basics of skin, hair, and nail health. Using the American Academy of Dermatology’s <em>Good Skin Knowledge</em> modules, we deliver fun, hands-on lessons that are grounded in science and created by dermatology experts.
+                        </p>
+                        <p>
+                            Our sessions cover daily care, sun safety, healthy habits, and common skin conditions. Students learn in an interactive, supportive setting that builds confidence and celebrates diversity.
+                        </p>
+                        <p>
+                            We believe every child should feel proud in their own skin and have the knowledge to keep it healthy for life.
+                        </p>
+                    </div>
                 </section>
 
                 <section
                     data-aos="fade-up"
                     data-aos-delay="200"
-                    data-aos-duration="400">
+                    data-aos-duration="400"
+                >
                     <h2 className="text-2xl font-semibold mb-3">What We Teach</h2>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                        {[
-                            { icon: "🧒", title: "Skin Structure and Function", color: "bg-[#568A97]/80" },
-                            { icon: "🧴", title: "Sun Protection and Safety", color: "bg-[#947D9E]/80" },
-                            { icon: "💇‍♀️", title: "Hair & Scalp Care", color: "bg-[#D5C6B9]/80" },
-                            { icon: "🧼", title: "Skin Health (Acne, Eczema & more)", color: "bg-[#D5C6B9]/80" },
-                            { icon: "💅", title: "Nail Care", color: "bg-[#568A97]/80" },
-                            { icon: "🌈", title: "Confidence & Kindness", color: "bg-[#947D9E]/80" }
-                        ].map((item, idx) => (
-                            <div
-                                key={idx}
-                                className={`${item.color} rounded-xl shadow-lg p-4 flex flex-col items-center justify-center text-center  gap-2 h-28  transition-transform duration-300 hover:scale-[1.02]`}
-                                data-aos="fade-up"
-                                data-aos-delay={idx * 100}
-                            >
-                                <div className="text-3xl">{item.icon}</div>
-                                <div className=" text-gray-700 font-semibold text-base sm:text-base">{item.title}</div>
-                            </div>
-                        ))}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+                        <TeachCard
+                            icon="🧴"
+                            color="bg-[#568A97]/80"
+                            title="Skin Structure and Function"
+                            titleUrl="https://www.aad.org/public/public-health/good-skin-knowledge/lesson-plans/intro-to-skin"
+                            delay={0}
+                        />
+
+                        <TeachCard
+                            icon="☀️"
+                            color="bg-[#947D9E]/80"
+                            title="Sun Protection and Safety"
+                            titleUrl="https://www.aad.org/public/public-health/good-skin-knowledge/lesson-plans/sun-safety"
+                            delay={100}
+                        />
+
+                        <TeachCard
+                            icon="🫧"
+                            color="bg-[#D5C6B9]/80"
+                            title="Daily Skin Care"
+                            titleUrl="https://www.aad.org/public/public-health/good-skin-knowledge/lesson-plans/daily-skin-care"
+                            delay={200}
+                        />
+
+                        <TeachCard
+                            icon="💇‍♀️"
+                            color="bg-[#568A97]/80"
+                            title="Hair & Scalp Care"
+                            titleUrl="https://www.aad.org/public/public-health/good-skin-knowledge/lesson-plans/healthy-hair-and-nails"
+                            delay={300}
+                        />
+
+                        <TeachCard
+                            icon="💅"
+                            color="bg-[#947D9E]/80"
+                            title="Nail Care"
+                            titleUrl="https://www.aad.org/public/public-health/good-skin-knowledge/lesson-plans/healthy-hair-and-nails"
+                            delay={400}
+                        />
+
+                        <TeachCard
+                            icon="🧼"
+                            color="bg-[#D5C6B9]/80"
+                            delay={500}
+                            titleNode={
+                                <span>
+                                    Skin Health (
+                                    <a
+                                        href="https://www.aad.org/public/public-health/good-skin-knowledge/lesson-plans/about-acne"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="underline"
+                                    >
+                                        Acne
+                                    </a>
+                                    {", "}
+                                    <a
+                                        href="https://www.aad.org/public/public-health/good-skin-knowledge/lesson-plans/eczema"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="underline"
+                                    >
+                                        Eczema
+                                    </a>
+                                    {" and More)"}
+                                </span>
+                            }
+                        />
+
+                        <TeachCard
+                            icon="🐞"
+                            color="bg-[#568A97]/80"
+                            title="Bug Bites and Poisonous Plants"
+                            titleUrl="https://www.aad.org/public/public-health/good-skin-knowledge/lesson-plans/bug-bites-poisonous-plants"
+                            delay={600}
+                        />
+
+                        <TeachCard
+                            icon="🌈"
+                            color="bg-[#947D9E]/80"
+                            title="Confidence and Kindness"
+                            titleUrl="https://www.aad.org/public/public-health/good-skin-knowledge/lesson-plans/love-your-skin"
+                            delay={700}
+                        />
                     </div>
                 </section>
 
                 <div className="relative w-full mt-2">
-                    <div className="absolute inset-0 bg-gradient-to-br from-[#F0E9D8] via-[#D5C6B9] to-[#947D9E] animate-gradient opacity-60 pointer-events-none blur-sm" aria-hidden="true" />
+                    <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_center,_#F0E9D8_25%,_#D5C6B9_98%,_#947D9E_100%)]
+                                    opacity-60 blur-sm pointer-events-none" 
+                            aria-hidden="true" />
                     <div className="relative z-10 w-full max-w-none mx-auto space-y-10 py-8">
                         <section
                             data-aos="fade-up"
@@ -87,12 +185,12 @@ export const HomeView = () => {
                             className="text-center">
                             <h2 className="text-2xl font-semibold mb-2">Partnerships</h2>
                             <h3 className="font-semibold text-base">Current Partners</h3>
-                            <ul className="list-disc list-inside text-center mb-4">
+                            <ul className="list-none list-inside text-center mb-4">
                                 <li>Telegraph Hill Neighborhood Center</li>
                                 <li>Boys and Girls Club Don Fisher Clubhouse</li>
                             </ul>
                             <h3 className="font-semibold text-base">Our Supporters</h3>
-                            <ul className="list-disc list-inside text-center">
+                            <ul className="list-none list-inside text-center">
                                 <li>UCSF Dermatology</li>
                                 <li>Center for Science, Education, & Outreach (CSEO)</li>
                             </ul>
--- src/views/InfoParentsView.jsx
+++ src/views/InfoParentsView.jsx
@@ -1,34 +1,34 @@
 import { useState } from "react"
-import SSKKidsLaughing from "../assets/SSK-kidslaughing.jpg"
+import SSKTeam from "../assets/SSK-crew-lighting.jpg"
 
 export const InfoParentsView = () => {
 
     const [isImageLoaded, setIsImageLoaded] = useState(false)
 
     return (
-        <div className="flex flex-col items-center bg-[#F0E9D8] min-h-screen overflow-hidden">
+        <div className="flex flex-col items-center min-h-screen overflow-hidden">
             <div className="relative w-full -mt-4">
                 <div
                     className={`absolute inset-0 w-full h-full bg-[#F0E9D8] transition-opacity duration-700 
                         ${isImageLoaded ? 'opacity-0' : 'opacity-100'}`}
                 />
                 <img
-                    src={SSKKidsLaughing}
-                    alt="Group of smiling kids outdoors"
+                    src={SSKTeam}
+                    alt="Group of team leaders"
                     onLoad={() => setIsImageLoaded(true)}
                     className={`w-full object-cover max-h-[600px] transition-opacity duration-700 
                         ${isImageLoaded ? 'opacity-100' : 'opacity-0'}`}
                 />
 
-                <div className="absolute top-4 left-8 w-[85%] sm:left-20 sm:w-[75%] md:left-24 md:w-[65%] md:max-w-2xl">
+                <div className="absolute z-10 bottom-8 left-8 w-[100%] sm:left-12 md:bottom-12 md:left-12 lg:left-24 lg:bottom-14">
                     <p
-                        className="text-md sm:text-xl md:text-3xl lg:text-4xl text-white drop-shadow-[0_1px_2px_rgba(0,0,0,0.15)] pt-5 sm:pt-5 md:pt-5"
+                        className="text-md sm:text-md md:text-2xl lg:text-3xl text-white drop-shadow-[0_1px_2px_rgba(0,0,0,0.15)] pt-5 sm:pt-5 md:pt-5"
                         data-aos="fade-up"
                         data-aos-delay="300"
                         data-aos-duration="1000"
                     >
-                        An educational afterschool program on dermatology, diversity,<br />
-                        and self-esteem.
+                        An educational afterschool program on<br />
+                        dermatology taught by UCSF medical students.
                     </p>
                 </div>
                 <div className="absolute bottom-0 left-0 w-full h-8 bg-gradient-to-b from-transparent to-[#F0E9D8] pointer-events-none" />
@@ -41,14 +41,16 @@ export const InfoParentsView = () => {
                     <h1 className="whitespace-nowrap leading-tight text-2xl sm:text-2xl font-semibold mb-4 -mt-4">
                         What is Safe Skin Kids?
                     </h1>
-
                     <p className="leading-relaxed">
-                        Safe Skin Kids is a free, UCSF medical
-                        student-led afterschool program that teaches children ages 8 to 13
-                        about skin, hair, and nail health. Using hands-on activities and
-                        inclusive materials, we help kids feel confident in their appearance,
-                        learn healthy habits, and explore the basics of dermatology in a fun
-                        and age-appropriate way.
+                        Safe Skin Kids is a free afterschool program that teaches children ages 8–13 about skin, hair
+                        and nail health through hands-on lessons from the American Academy of Dermatology’s
+                        <em> Good Skin Knowledge</em> modules. Students build confidence, learn healthy habits and see
+                        the beauty in every tone and texture.
+                    </p>
+                    <p className="leading-relaxed mt-4">
+                        We launched at Telegraph Hill Neighborhood Center and now partner with the Boys &amp; Girls Club
+                        Don Fisher Clubhouse to reach more kids in our community. By celebrating diversity and creating
+                        a space for belonging, we help every child feel proud in their own skin.
                     </p>
                 </section>
 
