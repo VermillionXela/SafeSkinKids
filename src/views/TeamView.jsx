b53b78b89395271411f7b9e16c0be2148a43a778
tswise <tswise@gmail.com>
2025-12-16 20:07:12 -0800

    Update TeamView.jsx
    added new team member bios and IMMEDIATELY committed to main because I am MOVING FAST and BREAKING THINGS (crying)

--- src/views/TeamView.jsx
+++ src/views/TeamView.jsx
@@ -2,6 +2,12 @@ import korenaImg from '../assets/korena.jpg'
 import floraImg from '../assets/flora.jpg'
 import meiyiImg from '../assets/meiyi.jpg'
 import mirandaImg from '../assets/miranda.jpg'
+import anuoluwaImg from '../assets/AnuAyeni.jpg'
+import folahanmiImg from '../assets/FolahanmiAsebiomo.jpg'
+import kellyImg from '../assets/KellyGocke.jpg'
+import kwekuImg from '../assets/KwekuDjan.jpg'
+import joshImg from '../assets/JoshPerez.jpg'
+import saraiImg from '../assets/SaraiOrtega.jpg'
 
 export const TeamView = () => {
     const teamMembers = [
@@ -42,6 +48,65 @@ export const TeamView = () => {
             description: `Hi everyone! I'm Miranda, a first-year medical student at UCSF who is originally from Palo Alto, CA and one of the lead instructors for Safe Skin Kids this year.\n
             I was motivated to join SSK because of my overlapping interests in dermatology, health equity, community engagement, and education and mentorship. As someone who has always been passionate about teaching as a way to empower others, I am excited to deliver meaningful, relevant, and accessible education on skin health, especially for young and adolescent individuals whose self-identities and self-confidence can rapidly change. I hope to teach students to feel more confident in their own skin and to have the tools to take better care of their skin throughout their lives.`,
             image: mirandaImg,
+        },
+        {
+            id: 5,
+            name: 'Kelly Gocke',
+            role: 'Lead Instructor',
+            description: `Hi! My name is Kelly Gocke and I’m a first-year medical student at UCSF.\n 
+            I’m originally from Fresno, California and majored in Psychobiology at UCLA before starting medical school.\n 
+            I’m currently a Lead Instructor for SSK and have enjoyed fostering a welcoming and inclusive environment while building relationships with the students throughout the course of the program.\n
+            I initially joined SSK because I am very passionate about health education as a means to reduce disparities in healthcare access and preventative care.\n
+            My favorite part of teaching through SSK has been hearing the students’ perspectives on topics and having the opportunity to debunk harmful myths or stereotypes in order to promote skin safety and inclusivity.`,
+            image: kellyImg,
+        },
+        {
+            id: 6,
+            name: 'Sarai Ortega',
+            role: 'Lead Instructor',
+            description: `I am a first-year medical student at UCSF in PRIME-US (Program in Medical Education for the Urban Underserved).\n
+            I am from Camarillo, CA and grew up in a Mexican-Guatemalan household.\n I serve as a lead instructor for Safe Skin Kids and am excited to help create a positive learning environment for SSK students and instructors.`,
+            image: saraiImg,
+        },
+        {
+            id: 7,
+            name: 'Josh Perez',
+            role: 'Director of Volunteer Recruitment',
+            description: `Hi! My name is Josh Perez (he/him), and I am a first-year medical student at UCSF.\n
+            I grew up in Stockton, California, and spent six years living in Los Angeles during my undergraduate studies.\n
+            At UCLA, I earned my Bachelor of Science in Microbiology, Immunology, and Molecular Genetics (MIMG).\n As the Director of Volunteer Recruitment in Safe Skin Kids, I am very excited to work towards growing the continuity of this program and add to its successes.\n
+            Beyond school, I love hosting pop-up coffee shops, having friends over for dinner, or going on runs around Golden Gate Park!`,
+            image: joshImg,
+        },
+        {
+            id: 8,
+            name: 'Kweku Djan',
+            role: 'Classroom Instructor',
+            description: `My name is Kweku Djan, and I am a fourth-year medical student at UCSF.\n
+            I grew up in Sacramento and went to Yale University for undergrad before coming to UCSF for medical school.\n
+            I have done multiple Safe Skin Kids sessions now and really enjoy the opportunity to expose kids to the medical profession early on.\n
+            There are not a ton of opportunities for teaching and mentorship of such young people, so it always reinvigorates me to be able to hang out with the kids and teach them about hair, skin, and nails, particularly in a way that is inclusive of all ethnicities.\n
+            As a future family medicine physician, knowing the basics of dermatology and teaching my patients-- which include kids-- about best practices for taking care of their bodies is integral.\n Working with SSK has given me the opportunity to sharpen those education skills and has even taught me new things.`,
+            image: kwekuImg,
+        },
+        {
+            id: 9,
+            name: 'Anuoluwa Ayeni',
+            role: 'Classroom Instructor',
+            description: `My name is Anuoluwa Ayeni, and I am a fourth-year medical student at UCSF.\n
+            I serve as an instructor with Safe Skin Kids. My interests include pediatric health disparities, pediatric dermatology, and any form of community involvement.\n
+            I wanted to get involved with SSK because it combines two of my favorite things when it comes to medicine: kids & accessible education!`,
+            image: anuoluwaImg,
+        },
+        {
+            id: 10,
+            name: 'Folahanmi Asebiomo',
+            role: 'Classroom Instructor',
+            description: `My name is Folahanmi Asebiomo, and I was born and raised in South Georgia before moving to California.\n
+            I’m now an MS1 at UCSF medical school.\n
+            I love spending free time on my “granny hobbies” like baking, crocheting, and needlework;\n
+            I also love to hang out with friends and watch dramatic reality TV.`,
+            image: folahanmiImg,
         }
     ]
 
